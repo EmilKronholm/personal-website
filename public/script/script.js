8d1b5b0f5be49f99b57bb29169c65bf0b1205ad7
ekkecode <emil.kronholm@gmail.com>
2025-02-08 23:29:34 +0100

    Script to dynamically move header based on mouse

--- public/script/script.js
+++ public/script/script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+    document.addEventListener('mousemove', (pos) => {
+    const headerContent = document.getElementById('header-content');
+
+    const normalizedDistanceToCenter = (x, y) => {
+        const constant = 40;
+        const xNormalizedDistance = (window.innerWidth/2 - x) / (window.innerWidth/2) * constant;
+        const yNormalizedDistance = (window.innerHeight/2 - y) / (window.innerHeight/2) * constant;
+        return { x: xNormalizedDistance, y: yNormalizedDistance };
+    }
+
+    const r = normalizedDistanceToCenter(pos.clientX, pos.clientY);
+    headerContent.style.transform = `translate(${r.x}px, ${r.y}px)`;
+});
