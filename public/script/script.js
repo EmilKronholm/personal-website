55cfea08f29b906db02708fa9b4b583fb1789b16
EmilKronholm <emil.kronholm@gmail.com>
2025-02-10 22:00:15 +0100

    Only add event listener on bigger screens
    (Also some formatting)

--- public/script/script.js
+++ public/script/script.js
@@ -1,13 +1,17 @@
-    document.addEventListener('mousemove', (pos) => {
-    const headerContent = document.getElementById('header-content');
+if (screen.width >= 768) {
+  document.addEventListener("mousemove", (pos) => {
+    const headerContent = document.getElementById("header-content");
 
     const normalizedDistanceToCenter = (x, y) => {
-        const constant = 40;
-        const xNormalizedDistance = (window.innerWidth/2 - x) / (window.innerWidth/2) * constant;
-        const yNormalizedDistance = (window.innerHeight/2 - y) / (window.innerHeight/2) * constant;
-        return { x: xNormalizedDistance, y: yNormalizedDistance };
-    }
+      const constant = 40;
+      const xNormalizedDistance =
+        ((window.innerWidth / 2 - x) / (window.innerWidth / 2)) * constant;
+      const yNormalizedDistance =
+        ((window.innerHeight / 2 - y) / (window.innerHeight / 2)) * constant;
+      return { x: xNormalizedDistance, y: yNormalizedDistance };
+    };
 
     const r = normalizedDistanceToCenter(pos.clientX, pos.clientY);
     headerContent.style.transform = `translate(${r.x}px, ${r.y}px)`;
-});
+  });
+}
