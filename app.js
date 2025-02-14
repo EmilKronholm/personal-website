60572d765a92dac016c68a1dbc3f0494c56ff46a
ekkecode <emil.kronholm@gmail.com>
2025-02-14 10:56:29 +0100

    Revert "Merge pull request #2 from EmilKronholm/development-goiboet"
    This reverts commit cf518b63619239bb77d7700de80e2523ffeb714c, reversing
    changes made to 9036616a914f765aba0ecfc2fe3960c695ff7e05.

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-const cors = require("cors")
 
 const app = express();
 const PORT = 3000;
-
-app.use(cors())
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,10 +11,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'ui/index.html'));
 });
-
-//Morfars lilla experiment
-const goiboet = require("./goiboet/goiboet-router")
-app.use(goiboet);
 
 // Start server
 app.listen(PORT, () => {
--- goiboet/readme.txt
+++
@@ -1,6 +0,0 @@
-This section is a voting system about if Tv4 ads are good or bad.
-As a request by my grandpa, I have quickly created him a voting app.
-
-This does not follow best practices and is unsafe for large traffic (due to plain writing into json file)
-
-I hope you enjoy it!
