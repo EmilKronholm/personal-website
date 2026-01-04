7a5df915a2956a0281e2bc71df75db0a2bde24e8
emilkronholm <emil.kronholm@gmail.com>
2026-01-04 04:02:49 +0100

    Remove cors
    Remove cors

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-const cors = require("cors")
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors())
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
